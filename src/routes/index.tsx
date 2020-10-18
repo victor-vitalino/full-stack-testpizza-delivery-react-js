29dc24695425de89d6bed9b74898c2b3f7162dfd
Victor Vitalino <victorvitalino237@gmail.com>
2020-10-18 13:21:45 -0300

    admin routes and user routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,17 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import user from "./user.json";
 
 import UserHeader from "../components/UserHeader";
+import AdminHeader from "../components/AdminHeader";
 
 // import { Container } from './styles';
 import SignIn from "../pages/SignIn";
-import Dashboard from "../pages/Dashboard";
+import Dashboard from "../pages/AdminPages/Dashboard";
+import Profile from "../pages/AdminPages/Profile";
 
-import PizzaMenu from "../pages/PizzaMenu";
-import UserOrders from "../pages/UserOrders";
-import UserCart from "../pages/UserCart";
+import PizzaMenu from "../pages/UserPages/PizzaMenu";
+import UserOrders from "../pages/UserPages/UserOrders";
+import UserCart from "../pages/UserPages/UserCart";
+import NewPizza from "../pages/AdminPages/NewPizza";
 
 const Router = () => {
   const { signed, admin } = user;
@@ -21,6 +24,7 @@ const Router = () => {
       {!signed ? (
         <Switch>
           <Route path="/" exact component={SignIn} />
+          <Route path="*" render={() => <Redirect to="/" />} />
         </Switch>
       ) : null}
 
@@ -31,13 +35,20 @@ const Router = () => {
             <Route path="/pizza-menu" component={PizzaMenu} />
             <Route path="/my-orders" component={UserOrders} />
             <Route path="/cart" component={UserCart} />
+            <Route path="*" render={() => <Redirect to="/pizza-menu" />} />
           </Switch>
         </>
       ) : null}
       {signed && admin ? (
-        <Switch>
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
+        <>
+          <AdminHeader />
+          <Switch>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/new-pizza" component={NewPizza} />
+            <Route path="*" render={() => <Redirect to="/dashboard" />} />
+          </Switch>
+        </>
       ) : null}
     </>
   );
