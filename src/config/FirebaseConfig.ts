bd3035e6d5f6532495fbc506c216a69d92d2e433
Victor Vitalino <62887252+victor-vitalino@users.noreply.github.com>
2020-11-10 21:57:06 -0300

    Update FirebaseConfig.ts

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyOdLQbM-Lgrr2uCeJG4Br2rUBvnK4s2s",
-  authDomain: "pizza4me-e8db3.firebaseapp.com",
-  databaseURL: "https://pizza4me-e8db3.firebaseio.com",
-  projectId: "pizza4me-e8db3",
-  storageBucket: "pizza4me-e8db3.appspot.com",
-  messagingSenderId: "84819435880",
-  appId: "1:84819435880:web:5c3814feaedcd653a0f6dd",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 export default firebase.initializeApp(firebaseConfig);
